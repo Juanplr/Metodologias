bd6bdf658746fa61f89558c3dcd600fdfef2fba7
ManuelInlo <iniguezlopez.devt@gmail.com>
2023-10-04 07:44:09 -0600

    version1

---
+++ src/proyectometodologias/ProyectoMetodologias.java
@@ -0,0 +1,32 @@
+package proyectometodologias;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author inigu
+ */
+public class ProyectoMetodologias extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
